f13ca63e43ec3b44ee88dbe7f748c1172a12011c
Ahranna Araujo <ahrannaaraujo@Ahrannas-MacBook-Air.local>
2022-12-23 19:10:52 +0000

    fixing ranking

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -4,7 +4,7 @@ export async function getRanking (req, res) {
  
     const rankingResult = await connection.query(`SELECT users.id, users.name, COUNT(urls.url) AS "linkCount", SUM(urls."visitCount") AS "visitCount" FROM users
     LEFT JOIN urls ON urls."userId" = users.id
-    GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10`)
+    GROUP BY users.id ORDER BY "visitCount" DESC NULLS LAST LIMIT 10`)
 
    const ranking = rankingResult.rows.map((row) => ( { ...row, linkCount: Number(row.linkCount),visitCount: Number(row.visitCount)}))
    
